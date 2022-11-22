b1aa648955d96453d8735d6be1ddcabe714cf788
Konsta Hölttä <sooda@iki.fi>
2022-11-22 21:45:03 +0200

    pixel proxy: use HW SPI
    Hardware SPI gives a bit more speed than bitbanging the bus. Note that
    this requires a modification to the wiring between arduino and the lcd.
    
    Rather than SPI.transfer() that busyloops until done, write to SPDR
    directly and only wait for the bit fifo to be empty before sending new
    bits, not after. Also wait before latching the display data upon
    completion of a frame, but do that after Serial.write() to hide some
    latency there as well.

--- utils/pixel_map/proxy.ino
+++ utils/pixel_map/proxy.ino
@@ -1,12 +1,16 @@
-// directions as seen by the LCD
-#define LD_PIN 11
-#define CL_PIN 10
-#define DI_PIN 9 // LCD data input
+#include <SPI.h>
+
+// SPI: SS MOSI MISO SCK = 10 11 12 13
+#define SPI_DATA_PIN 11
+#define SPI_SCLK_PIN 13
+
+#define CL_PIN SPI_SCLK_PIN
+#define DI_PIN SPI_DATA_PIN
+#define LD_PIN 9
 #define FL_PIN 8
-#define DISPLAY_BITS 480
-#define DISPLAY_BYTES DISPLAY_BITS/8
 
 // Use timer interrupt to generate 64 Hz square wave for FL pin.
+// TODO: HW PWM on an "analog" pin.
 void setupTimer1() {
   // AVR Timer CTC Interrupts Calculator
   // v. 8
@@ -34,31 +38,22 @@ ISR(TIMER1_COMPA_vect) {
   digitalWrite(FL_PIN, digitalRead(FL_PIN) ^ 1);
 }
 
-void shiftBit(int b) {
-  digitalWrite(CL_PIN, HIGH);
-  //delayMicroseconds(1);
-  if (b) {
-    digitalWrite(DI_PIN, HIGH);
-  } else {
-    digitalWrite(DI_PIN, LOW);
-  }
-  //delayMicroseconds(1);
-  digitalWrite(CL_PIN, LOW);
-  //delayMicroseconds(1);
+inline void spi_wait_idle() {
+  while (!(SPSR & _BV(SPIF)))
+    ;
 }
 
 void latch() {
+  spi_wait_idle();
   digitalWrite(LD_PIN, HIGH);
-  delayMicroseconds(100);
+  // 'duino is slow enough so no explicit delay needed
+  //delayMicroseconds(1);
   digitalWrite(LD_PIN, LOW);
 }
 
-void clear() {
-  // clear out existing values
-  for (int i = 0; i < DISPLAY_BITS; i++) {
-    shiftBit(0);
-  }
-  latch();
+void spi_transmit(byte data) {
+  spi_wait_idle();
+  SPDR = data;
 }
 
 void setup() {
@@ -69,34 +64,31 @@ void setup() {
   digitalWrite(LD_PIN, LOW);
   setupTimer1();
 
-  Serial.begin(115200);
-  // Serial.begin(230400);
+  SPI.begin();
+  // CPOL=0, CPHA=1 (clock normally low, data sampled at trailing edge)
+  // 500 kHz rate not verified
+  SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
+  SPDR = 0; // mark the first SPIF for spi_wait_idle
 
-  clear();
+  Serial.begin(115200);
+  // faster uart seems to be glitchy
+  // Serial.begin(230400);
 }
 
-int index = 0;
+static int index = 0;
 
 void loop() {
   int incomingByte = Serial.read();
   if (incomingByte != -1) {
     Serial.write(index); // load-bearing print statement
-    byte compress = 1;
-    if (compress) {
-      byte x = incomingByte;
-      for (byte c = 0; c < 8; c++) {
-        shiftBit((x & 0x80) != 0);
-        x <<= 1;
-      }
-      index += 8;
-    } else {
-      shiftBit(incomingByte != 0);
-      index++;
-    }
-    if (index == 480) {
-      latch();
-      Serial.write('.');
+    byte x = incomingByte;
+    spi_transmit(x);
+    index++;
+
+    if (index == 60) {
       index = 0;
+      Serial.write('.');
+      latch();
     }
   }
 }
